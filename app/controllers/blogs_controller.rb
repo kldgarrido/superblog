929be3bc15da538e3415d3827c4674d9c9301c32
kld <kldgarrido.com>
2017-06-15 13:59:16 -0500

    add blogs#index

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,2 +1,9 @@
 class BlogsController < ApplicationController
+
+	def index
+		@blogs = Blog.order('updated_at DESC')
+	end
+
+	
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,5 +6,5 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-blogs = Blog.create([{title: 'CRUD', body:'En informática, CRUD es el acrónimo de "Crear, Leer, Actualizar y Borrar" (del original en inglés: Create, Read, Update and Delete), que se usa para referirse a las funciones básicas en bases de datos o la capa de persistencia en un software.'}])
+blogs = Blog.create([{title: 'CRUD', body:'En informática, CRUD es el acrónimo de "Crear, Leer, Actualizar y Borrar" (del original en inglés: Create, Read, Update and Delete), que se usa para referirse a las funciones básicas en bases de datos o la capa de persistencia en un software.'}, {title: 'Tom Clancy\'s The Division', body:'Tom Clancy\'s The Division is an online-only open world third-person shooter video game developed by Massive Entertainment and published by Ubisoft, with assistance from Red Storm Entertainment, for Microsoft Windows, PlayStation 4 and Xbox One'}])
 comments = Comment.create([{body:'is cool', blog: blogs[0] }]) 

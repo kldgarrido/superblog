ff1f8235c639ca6feedd8f0d3c50af492cd20d21
kld <kldgarrido.com>
2017-06-17 08:57:27 -0500

    add create function

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -6,8 +6,47 @@ class BlogsController < ApplicationController
 		@blogs = Blog.order('created_at DESC')
 	end
 
+	def new
+  		@blog = Blog.new
+	end
+
+	def edit
+  		@blog = Blog.find(params[:id])
+	end
+
 	def show
 		@blog = Blog.find(params[:id])
 	end
+
+	def create
+	  @blog = Blog.new(blog_params)
+	  @blog.user = current_user
+	  if @blog.save
+	    redirect_to blogs_path, notice: "El blog fue publicado con éxito"
+	  else
+	    render :new
+	  end
+	end
+
+	def update
+	  @blog = Blog.find(params[:id])
+	  if @blog.update(blog_params)
+	    redirect_to blogs_path, notice: "El blog ha sido modificado con éxito"
+	  else
+	    render :edit
+	  end
+	end
+
+	def destroy
+	  blog = Blog.find(params[:id])
+	  blog.destroy
+
+	  redirect_to blog_path, notice: "El blog fue eliminado con éxito"
+	end
+
+	private
+	  def blog_params
+	    params.require(:blog).permit(:title, :body)
+	  end
 
 end
--- app/helpers/blogs_helper.rb
+++ app/helpers/blogs_helper.rb
@@ -1,2 +1,5 @@
 module BlogsHelper
+	def form_title
+		@blog.new_record? ? "Publicar Blog" : "Modificar Blog"
+	end
 end
